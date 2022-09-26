0db5bb0c49fa9ffb73164f8cd24b37a58cceeacf
joaojjb <joaojjbcosta@gmail.com>
2022-09-26 18:13:40 -0300

    Segundo Commit

---
+++ Exercicio04Classes/src/Ingresso.java
@@ -0,0 +1,23 @@
+public class Ingresso {
+    private float valor;
+
+
+    public Ingresso() {
+        this.valor = 60;
+    }
+
+
+    public float getValor() {
+        return valor;
+    }
+
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+
+    public float imprimiValor(){
+        return this.valor;
+    }
+}
