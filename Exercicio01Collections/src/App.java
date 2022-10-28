98e806d9a09a23f73d5903bd777554161d2d20be
joaojjb <joaojjbcosta@gmail.com>
2022-10-28 08:41:42 -0300

    V1 Trabalho Arquivo Texto

--- Exercicio01Collections/src/App.java
+++ Exercicio01Collections/src/App.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class App{
     public static void main(String[] args) throws Exception {
         Randomizador random = new Randomizador();
-        
         ArrayList<Funcionario> listaFuncionario = new ArrayList<>();
         
-        
-
         for(int i = 0; i < 10; i ++){
             Funcionario funcionario = new Funcionario();
+            
             funcionario.setTelefone(random.geraTelefone());
             funcionario.setSalario(random.geraSalario());
             funcionario.setNome(random.geraNome());
---
+++ Trabalho Leitura Arquivo Texto/src/App.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+public class App {
+    public static void main(String[] args) throws Exception {
+        String var = "";
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("C:\Users\Joao\Desktop\VsCodeJavaProjects\TecnicasDePrograma-o\Trabalho Leitura Arquivo Texto\ArquivoTexto"))
+        }
+    }
+}
